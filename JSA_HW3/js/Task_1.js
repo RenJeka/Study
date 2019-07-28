259127e657f176125b7890e916959c1af50ee8e3
Ren Jeka <renjeka@gmail.com>
2019-07-29 02:15:01 +0300

    неправильно работает секундомер. Добавляет лишнюю единицу

--- JSA_HW3/js/Task_1.js
+++ JSA_HW3/js/Task_1.js
@@ -19,37 +19,37 @@ window.onload = function(){
 
 		if (counterMiliSec < 9) {
 
-			++counterMiliSec;
+			counterMiliSec++;
 			get("span6").innerHTML = counterMiliSec;
 
 		}else if(counterMiliSecT < 9){
-
-			++counterMiliSecT;
+			
 			counterMiliSec = 0;
+			counterMiliSecT++;
 			get("span5").innerHTML = counterMiliSecT;
 
 		}else if (counterSec < 9) {
-
-			++counterSec ;
+			
 			counterMiliSecT = 0;
+			counterSec++ ;
 			get("span4").innerHTML = counterSec;
 
 		}else if(counterSecT < 6){
-
-			++counterSecT ;
+			
 			counterSec = 0;
+			counterSecT++ ;
 			get("span3").innerHTML = counterSecT;
 
 		}else if (counterMin <9) {
-
-			++counterMin;
+			
 			counterSecT = 0;
+			counterMin++;
 			get("span2").innerHTML = counterMin;
 
 		}else {
-
-			++counterMinT;
+			
 			counterMin =0;
+			counterMinT++;
 			get("span1").innerHTML = counterMinT;
 		}
 		
@@ -71,12 +71,12 @@ window.onload = function(){
 	
 	function resetCount() {
 		clearInterval(intervalHandler);
-		var counterMiliSec = 0;
-		var counterMiliSecT = 0;
-		var counterSec = 0;
-		var counterSecT = 0;
-		var counterMin = 0;
-		var counterMinT = 0;
+		counterMiliSec = 0;
+		counterMiliSecT = 0;
+		counterSec = 0;
+		counterSecT = 0;
+		counterMin = 0;
+		counterMinT = 0;
 		get("span6").innerHTML = counterMiliSec;
 		get("span5").innerHTML = counterMiliSecT;
 		get("span4").innerHTML = counterSec;

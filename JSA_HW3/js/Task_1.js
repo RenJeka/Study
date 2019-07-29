071b424b27de0f6ed93a91da2824f0c37e45bf84
Ren Jeka <renjeka@gmail.com>
2019-07-29 10:39:35 +0300

    Рабочая версия 2. функция get не используется при каждой итерации. Так тратиться меньше ресурсов ПК

--- JSA_HW3/js/Task_1.js
+++ JSA_HW3/js/Task_1.js
@@ -9,68 +9,69 @@ window.onload = function(){
 	var intervalHandler;
 	var flag = false;
 
-/* 	var span6 = get("span6");
-	var span5 = get("span5");
-	var span4 = get("span4");
-	var span3 = get("span3");
-	var span2 = get("span2");
-	var span1 = get("span1"); */
-
 	// Функция, взята из 4-го примера по таймерам. Позволяет получить доступ к элементу, не записуя элемент в отдельно созданную пеоеменную.
+	// ♦ почему эту функцию нельзя поставить дальше, а обратиться к ней сейчас. Интерпритатор должен знать и перепрыгнуть на нужную функцию
 	var get = function (id) {
 		return document.getElementById(id);		
 	};
 
+	var span6 = get("span6");
+	var span5 = get("span5");
+	var span4 = get("span4");
+	var span3 = get("span3");
+	var span2 = get("span2");
+	var span1 = get("span1");
 
+	// функция-счетчик. Функция считает и записывает счетчики отдельно для каждой из 6-ти переменных :	(миллисекунд, десятых миллисекунд, секунд, десятых секунд, минут, десятых минут)
 	function count() {
 
 		if (counterMiliSec < 9) {
-
+			// счетчик (миллисекунд) считает до тех пор, пока он не достигнет 9-ти. (Аналогично для всех остальных 5-ти счетчиков)
 			counterMiliSec++;
-			get("span6").innerHTML = counterMiliSec;
+			span6.innerHTML = counterMiliSec;
 
 		}else if(counterMiliSecT < 9){
-			
+			// обнуляем все предыдущие счетчики (Аналогично для всех остальных 4-рех счетчиков)
 			counterMiliSec = 0;
-			get("span6").innerHTML = counterMiliSec;
+			span6.innerHTML = counterMiliSec;
 
 			counterMiliSecT++;
-			get("span5").innerHTML = counterMiliSecT;
+			span5.innerHTML = counterMiliSecT;
 
 		}else if (counterSec < 9) {
 			
 			counterMiliSecT = 0;
 			counterMiliSec = 0;
-			get("span6").innerHTML = counterMiliSec;
-			get("span5").innerHTML = counterMiliSecT;
+			span6.innerHTML = counterMiliSec;
+			span5.innerHTML = counterMiliSecT;
 
 			counterSec++ ;
-			get("span4").innerHTML = counterSec;
+			span4.innerHTML = counterSec;
 
 		}else if(counterSecT < 6){
 
 			counterMiliSecT = 0;
 			counterMiliSec = 0;
 			counterSec = 0;
-			get("span6").innerHTML = counterMiliSec;
-			get("span5").innerHTML = counterMiliSecT;
-			get("span4").innerHTML = counterSec;
+			span6.innerHTML = counterMiliSec;
+			span5.innerHTML = counterMiliSecT;
+			span4.innerHTML = counterSec;
 
 			counterSecT++ ;
-			get("span3").innerHTML = counterSecT;
+			span3.innerHTML = counterSecT;
 
 		}else if (counterMin <9) {
 			counterMiliSecT = 0;
 			counterMiliSec = 0;
 			counterSec = 0;
 			counterSecT = 0;
-			get("span6").innerHTML = counterMiliSec;
-			get("span5").innerHTML = counterMiliSecT;
-			get("span4").innerHTML = counterSec;
-			get("span3").innerHTML = counterSecT;
+			span6.innerHTML = counterMiliSec;
+			span5.innerHTML = counterMiliSecT;
+			span4.innerHTML = counterSec;
+			span3.innerHTML = counterSecT;
 
 			counterMin++;
-			get("span2").innerHTML = counterMin;
+			span2.innerHTML = counterMin;
 
 		}else {
 			counterMiliSecT = 0;
@@ -78,32 +79,33 @@ window.onload = function(){
 			counterSec = 0;
 			counterSecT = 0;
 			counterMin =0;
-			get("span6").innerHTML = counterMiliSec;
-			get("span5").innerHTML = counterMiliSecT;
-			get("span4").innerHTML = counterSec;
-			get("span3").innerHTML = counterSecT;
-			get("span2").innerHTML = counterMin;
+			span6.innerHTML = counterMiliSec;
+			span5.innerHTML = counterMiliSecT;
+			span4.innerHTML = counterSec;
+			span3.innerHTML = counterSecT;
+			span2.innerHTML = counterMin;
 
 			counterMinT++;
-			get("span1").innerHTML = counterMinT;
+			span1.innerHTML = counterMinT;
 		}
 		
 	}
 
+	// функция проверяет, если не запущен счетчик — запускает его. Проверка нужна, чтобы при повторном нажатии на кнопку "Старт" — счетчик не запустился повторно 
 	function startCount() {
 		if (!flag) {
 			flag = true;
 			intervalHandler = setInterval(count, 10);
 		}
-
-		
 	}
 
+	// функция останавливает счетчик
 	function stopCount() {
 		clearInterval(intervalHandler);
 		flag = false;
 	}
 	
+	// функция обнуляет счетчик
 	function resetCount() {
 		clearInterval(intervalHandler);
 		counterMiliSec = 0;
@@ -112,12 +114,12 @@ window.onload = function(){
 		counterSecT = 0;
 		counterMin = 0;
 		counterMinT = 0;
-		get("span6").innerHTML = counterMiliSec;
-		get("span5").innerHTML = counterMiliSecT;
-		get("span4").innerHTML = counterSec;
-		get("span3").innerHTML = counterSecT;
-		get("span2").innerHTML = counterMin;
-		get("span1").innerHTML = counterMinT;
+		span6.innerHTML = counterMiliSec;
+		span5.innerHTML = counterMiliSecT;
+		span4.innerHTML = counterSec;
+		span3.innerHTML = counterSecT;
+		span2.innerHTML = counterMin;
+		span1.innerHTML = counterMinT;
 		flag = false;
 	}
 

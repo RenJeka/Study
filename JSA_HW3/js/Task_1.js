71bbaeabf616a9d7080284ae5a2163eb0e5ee3dc
Ren Jeka <renjeka@gmail.com>
2019-07-29 10:17:16 +0300

    рабочий прототип. Необходимо улучшить get- функцию. Программа слишком часто обращается к ней.

--- JSA_HW3/js/Task_1.js
+++ JSA_HW3/js/Task_1.js
@@ -9,6 +9,13 @@ window.onload = function(){
 	var intervalHandler;
 	var flag = false;
 
+/* 	var span6 = get("span6");
+	var span5 = get("span5");
+	var span4 = get("span4");
+	var span3 = get("span3");
+	var span2 = get("span2");
+	var span1 = get("span1"); */
+
 	// Функция, взята из 4-го примера по таймерам. Позволяет получить доступ к элементу, не записуя элемент в отдельно созданную пеоеменную.
 	var get = function (id) {
 		return document.getElementById(id);		
@@ -25,30 +32,58 @@ window.onload = function(){
 		}else if(counterMiliSecT < 9){
 			
 			counterMiliSec = 0;
+			get("span6").innerHTML = counterMiliSec;
+
 			counterMiliSecT++;
 			get("span5").innerHTML = counterMiliSecT;
 
 		}else if (counterSec < 9) {
 			
 			counterMiliSecT = 0;
+			counterMiliSec = 0;
+			get("span6").innerHTML = counterMiliSec;
+			get("span5").innerHTML = counterMiliSecT;
+
 			counterSec++ ;
 			get("span4").innerHTML = counterSec;
 
 		}else if(counterSecT < 6){
-			
+
+			counterMiliSecT = 0;
+			counterMiliSec = 0;
 			counterSec = 0;
+			get("span6").innerHTML = counterMiliSec;
+			get("span5").innerHTML = counterMiliSecT;
+			get("span4").innerHTML = counterSec;
+
 			counterSecT++ ;
 			get("span3").innerHTML = counterSecT;
 
 		}else if (counterMin <9) {
-			
+			counterMiliSecT = 0;
+			counterMiliSec = 0;
+			counterSec = 0;
 			counterSecT = 0;
+			get("span6").innerHTML = counterMiliSec;
+			get("span5").innerHTML = counterMiliSecT;
+			get("span4").innerHTML = counterSec;
+			get("span3").innerHTML = counterSecT;
+
 			counterMin++;
 			get("span2").innerHTML = counterMin;
 
 		}else {
-			
+			counterMiliSecT = 0;
+			counterMiliSec = 0;
+			counterSec = 0;
+			counterSecT = 0;
 			counterMin =0;
+			get("span6").innerHTML = counterMiliSec;
+			get("span5").innerHTML = counterMiliSecT;
+			get("span4").innerHTML = counterSec;
+			get("span3").innerHTML = counterSecT;
+			get("span2").innerHTML = counterMin;
+
 			counterMinT++;
 			get("span1").innerHTML = counterMinT;
 		}

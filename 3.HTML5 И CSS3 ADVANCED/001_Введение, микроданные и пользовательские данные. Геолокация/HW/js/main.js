59b3f730a42617b06ec12cc2b8de145a0901d157
Ren Jeka <renjeka@gmail.com>
2019-09-03 20:51:11 +0300

    мелкие исправления в 1-м задании. Не работала карта для 2 отеля

--- 3.HTML5 И CSS3 ADVANCED/001_Введение, микроданные и пользовательские данные. Геолокация/HW/js/main.js
+++ 3.HTML5 И CSS3 ADVANCED/001_Введение, микроданные и пользовательские данные. Геолокация/HW/js/main.js
@@ -9,7 +9,7 @@ window.addEventListener("load", function () {
 	var hotels ={
 		hotel_1:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3540.7897323667758!2d-82.60939350979717!3d27.444662989156537!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88c3142a5481c95d%3A0x902f1690d2fb1069!2sLegacy%20Hotel%20at%20IMG%20Academy!5e0!3m2!1sru!2sua!4v1567525097046!5m2!1sru!2sua",
 
-		hotel_2:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d210423.95347783086!2d-48.81893141392136!3d-17.828243029444135!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94a0cb7902dd0683%3A0xb9300b97cb892326!2sIMG%20Hotel&#39;s%20Rio%20Quente!5e0!3m2!1sru!2sua!4v1567528680223!5m2!1sru!2sua",
+		hotel_2:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1343.2525274817747!2d-48.75464736652733!3d-17.777974108799693!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94a0cb7786f1e01f%3A0x8decbda7b8ab08f2!2sRio%20Quente%20Cristal%20Resort!5e0!3m2!1sru!2sua!4v1567532675954!5m2!1sru!2sua",
 
 		hotel_3:"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d8666.77249831367!2d-82.69641271108156!3d27.46323994241093!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x98f2808e17efb1b5!2sBridgeWalk%20-%20A%20Landmark%20Resort!5e0!3m2!1sru!2sua!4v1567529142691!5m2!1sru!2sua",
 
@@ -21,9 +21,13 @@ window.addEventListener("load", function () {
 
 
 
+
+
+
+	// Задаем высоту нашей карты через высоту окна текущего браузера 
 	iframe.setAttribute("height", window.innerHeight );
 
-
+	// Перебор всех кнопок чтобы повесить обработчик события
 	for (i = 0; i<arrayOfBtn.length ; i++) {
 		
 		arrayOfBtn[i].addEventListener("click", function () {

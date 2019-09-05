c0e45732f5efbe917832742851abb652d158e8bb
RenJeka <renjeka@gmail.com>
2019-09-05 20:05:47 +0300

    Добавлена ф-я, которая рисует линию в канвасе

--- 3.HTML5 И CSS3 ADVANCED/002_Canvas/HW/js/home_work.js
+++ 3.HTML5 И CSS3 ADVANCED/002_Canvas/HW/js/home_work.js
@@ -28,14 +28,30 @@ window.addEventListener("load", function () {
     ctx.fill();
     ctx.stroke();
 
+    var currentX;
+    var currentY;
+
+    function shiftLine(shiftX, shiftY) {
+
+        currentX = currentX + shiftX;
+        currentY = currentY + shiftY;
+        ctx2.lineTo(currentX, currentY);
+
+    }
 
     var shift1X = 120;
     var shift1Y = 75;
     var ctx2 = myCanvas.getContext("2d");
-    ctx2.moveTo(startX +shift1X,startY+shift1Y);
-    ctx2.lineWidth = 3;
+    // ctx2.moveTo(startX +shift1X,startY+shift1Y);
 
-    ctx2.lineTo(100,100);
+    currentX =startX +shift1X;
+    currentY = startY+shift1Y;
+    ctx2.moveTo(currentX,currentY);
+    shiftLine(50,50);
+
+
+    ctx2.lineWidth = 3;
+    console.dir(ctx2);
     ctx2.stroke();
 
 

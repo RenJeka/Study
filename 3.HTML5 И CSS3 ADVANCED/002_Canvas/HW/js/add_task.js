4aa07f9a30099ca5a3393c39cee6c19781111a2f
RenJeka <renjeka@gmail.com>
2019-09-05 16:15:24 +0300

    Сделано доп. задание. Версия 1.0

---
+++ 3.HTML5 И CSS3 ADVANCED/002_Canvas/HW/js/add_task.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", function () {
+    
+    var myCanvas = this.document.querySelector("canvas");
+    
+    var myContext =  myCanvas.getContext("2d");
+
+    myContext.save();
+    myContext.translate(200, 200);
+
+    for (let i = 0; i < 10; i++) {
+        var  shiftX = i * 10 + 10;
+        myContext.moveTo(shiftX,0);
+        myContext.lineTo(shiftX, 110);
+        myContext.stroke();
+        
+    }
+    for (let i = 0; i < 10; i++) {
+        var  shiftY = i * 10 + 10;
+        myContext.moveTo(0,shiftY);
+        myContext.lineTo(110, shiftY);
+        myContext.stroke();
+
+    }
+
+    
+    myContext.restore();
+
+
+})

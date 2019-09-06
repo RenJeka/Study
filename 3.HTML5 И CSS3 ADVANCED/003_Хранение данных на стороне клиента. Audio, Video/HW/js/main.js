87d033fbb0d6ec10eccb32916bdbf35368099130
RenJeka <renjeka@gmail.com>
2019-09-06 18:08:15 +0300

    Полный, но не рабочий проэкт

--- 3.HTML5 И CSS3 ADVANCED/003_Хранение данных на стороне клиента. Audio, Video/HW/js/main.js
+++ 3.HTML5 И CSS3 ADVANCED/003_Хранение данных на стороне клиента. Audio, Video/HW/js/main.js
@@ -17,6 +17,38 @@ window.addEventListener("load", function () {
         var worker3 = new Worker ("worker3.js");
         var worker4 = new Worker ("worker4.js");
 
+        btn1.addEventListener("click", function () {
+            worker1.addEventListener("message", function (e) {
+                output.innerHTML += e.data + "</br>";
+            })
+            worker1.postMessage("some data");
+        });
+
+
+        btn2.addEventListener("click", function () {
+            worker2.addEventListener("message", function (e) {
+                output.innerHTML += e.data + "</br>";
+            })
+            worker2.postMessage("some data");
+        });
+
+
+        btn3.addEventListener("click", function () {
+            worker3.addEventListener("message", function (e) {
+                output.innerHTML += e.data + "</br>";
+            })
+            worker3.postMessage("some data");
+        });
+
+
+        btn4.addEventListener("click", function () {
+            worker4.addEventListener("message", function (e) {
+                output.innerHTML += e.data + "</br>";
+            })
+            worker4.postMessage("some data");
+        });
+
+
 
 
 
---
+++ 3.HTML5 И CSS3 ADVANCED/003_Хранение данных на стороне клиента. Audio, Video/HW/js/worker1.js
@@ -0,0 +1,3 @@
+addEventListener("message", function (e) {
+   postMessage(e.data + "воркер-1  успешно завершил вычисления") 
+});

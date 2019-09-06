54158611978216d54babf37d7fa00aab21ad1ba7
RenJeka <renjeka@gmail.com>
2019-09-06 17:54:52 +0300

    Создание пустой структуры с обработчиками событий

--- 3.HTML5 И CSS3 ADVANCED/003_Хранение данных на стороне клиента. Audio, Video/HW/js/main.js
+++ 3.HTML5 И CSS3 ADVANCED/003_Хранение данных на стороне клиента. Audio, Video/HW/js/main.js
@@ -1,3 +1,24 @@
 window.addEventListener("load", function () {
-    
+    function $(selector) {
+        return document.getElementById(selector);
+    }
+
+    var 
+        inp1    = $("number_1"),
+        inp2    = $("number_2"),
+        btn1    = $("number_2"),
+        btn2    = $("number_2"),
+        btn3    = $("number_2"),
+        btn4    = $("number_2"),
+        output  = $("output");
+
+        var worker1 = new Worker ("worker1.js");
+        var worker2 = new Worker ("worker2.js");
+        var worker3 = new Worker ("worker3.js");
+        var worker4 = new Worker ("worker4.js");
+
+
+
+
+
 });

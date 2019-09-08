7f02e1a56acbffeccb4251c224ec1ab0854c1ab7
Ren Jeka <renjeka@gmail.com>
2019-09-08 21:39:23 +0300

    ДЗ-3. Домашняя работа №1. Готовый флаг с листком. Необходимо подправить тень

--- 3.HTML5 И CSS3 ADVANCED/002_Canvas/HW/js/home_work.js
+++ 3.HTML5 И CSS3 ADVANCED/002_Canvas/HW/js/home_work.js
@@ -21,21 +21,16 @@ window.addEventListener("load", function () {
     ctx.fillStyle = myGradient;
     
     //=============SHADOW============
-    // ctx.shadowColor = "black";
-    // ctx.shadowOffsetX = 5;
-    // ctx.shadowOffsetY = 5;
-    // ctx.shadowBlur = 3;
+    ctx.shadowColor = "gray";
+    ctx.shadowOffsetX = 2;
+    ctx.shadowOffsetY = 2;
+    ctx.shadowBlur = 10;
+    // ctx.globalCompositeOperation = "destination-out";
     //===============================
 
 
     //=========ФЛАГ=========================
     ctx.lineWidth = 0.01;
-
-    //=========ТРАНСФОРМАЦИЯ======
-    // TODO == Почему трансформируется все ?
-    // ctx.translate(50,50);
-    // ctx.rotate(.5);
-    // ctx.scale(2,2);
 
     ctx.beginPath();
     ctx.moveTo(startX+shiftX, startY+shiftY);
@@ -43,6 +38,8 @@ window.addEventListener("load", function () {
     ctx.lineTo(414,278 );
     ctx.bezierCurveTo(390 , 230 , 230 , 330 , 160 , 291 );
     ctx.closePath();
+    // ctx2.globalCompositeOperation = "sourse-out";
+    // ctx.globalCompositeOperation='overlay';
     ctx.fill();
     ctx.stroke();   
     //======================================
@@ -58,18 +55,52 @@ window.addEventListener("load", function () {
     }
 
     var ctx2 = myCanvas.getContext("2d");
-    // ctx2.rotate(.2);
     // ctx2.scale(-1, 1);
     // ctx2.translate(100, 100);
     var shift1X = 130;
     var shift1Y = 90;
     // ctx2.moveTo(startX +shift1X,startY+shift1Y);
+    // ctx2.transform(1,0.05,0.05,1,1,1);
+    
+    ctx.shadowColor = "red";
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0;
+    ctx.shadowBlur = 0;
+
     ctx2.save();
-    currentX =startX +shift1X;
-    currentY = startY+shift1Y;
+    currentX = 0;
+    currentY = 0;
+    ctx2.translate(280,240);
+    ctx2.rotate(-0.5);
+    ctx2.transform(1,0.2,-0.5,1,1,1);
+    ctx2.scale(-1, 0.89);
+    ctx2.beginPath();
+    ctx2.moveTo(2,-6);
+    shiftLine(30,0);
+    shiftLine(-3,-10);
+    shiftLine(15,-20);
+    shiftLine(-5,-3);
+    shiftLine(5,-15);
+    shiftLine(-15,2);
+    shiftLine(-2,-7);
+    shiftLine(-15,20);
+    shiftLine(7,-35);
+    shiftLine(-8,4);
+    shiftLine(-10,-27);
+    shiftLine(0,120);
+    shiftLine(4,0);
+    ctx2.closePath();
+    ctx2.restore();
+    ctx2.fillStyle="red";
+    // ctx2.globalCompositeOperation = "destination-in";
+    ctx2.fill();
+    ctx2.stroke();
 
-    // ctx2.transform(0.1,0,0,1,0,0);
 
+    ctx2.save();
+    currentX = 0;
+    currentY = 0;
+    ctx2.translate(280,235);
     ctx2.beginPath();
     ctx2.moveTo(currentX,currentY);
     shiftLine(30,0);
@@ -85,24 +116,17 @@ window.addEventListener("load", function () {
     shiftLine(-10,-20);
     shiftLine(0,120);
     shiftLine(4,0);
-    // shiftLine(0,-35);
     ctx2.closePath();
-
-
-
-    
+    ctx2.restore();
     ctx2.fillStyle="red";
     ctx2.fill();
-    
-    // TRANSFORMATION
-    // ctx2.transform(1,33,33,1,0,0)
-    // ctx2.rotate(.2);
-    // ctx2.scale(50,50);
-    // ctx2.translate(100, 100);
-    
-    // ctx2.lineWidth = 0.5;
-    console.dir(ctx2);
     ctx2.stroke();
+
+    
+
+    
+    console.dir(ctx2);
+
     //======================================
 
 

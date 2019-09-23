bdc8b962f6e2ae237e05f58de1b2e7bcfb4a14fa
Ren Jeka <renjeka@gmail.com>
2019-09-23 11:50:43 +0300

    Добавил тестовый проект №3 в папке GRID / Flex-box

---
+++ HTML5 and CSS3 ADVANCED/004_Принципы построения разметки, Flex, Grid, Шаблоны сайтов/004_Samples/Test/3/main.js
@@ -0,0 +1,14 @@
+let myObject = {
+	a:"hello",
+	b: 380995552266,
+	say: function () {
+		console.log("Hello there!");
+	}
+
+}
+
+console.log("%O", myObject);
+
+let myFunk (a= "lll", b = "sjkdn") => a+b;
+
+console.log(myFunk());

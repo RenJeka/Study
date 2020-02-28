e9713a8b8ebd2e3459b750f719183d11a7fc6391
renjeka <renjeka@gmail.com>
2020-02-28 22:14:32 +0200

    добавлены ДЗ ITVDN  TypeScript

---
+++ TypeScript/Test/3/main.js
@@ -0,0 +1,4 @@
+var firstVariable = 555;
+console.log(firstVariable.toFixed);
+var myStringVar = "122";
+console.log(parseInt(myStringVar));
---
+++ TypeScript/Test/3/main.ts
@@ -0,0 +1,7 @@
+let firstVariable: number = 555;
+
+console.log(firstVariable.toFixed);
+
+let myStringVar: string = "122";
+
+console.log(parseInt(myStringVar));

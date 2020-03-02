0e8e880352d7ade6eba7724d57332ddf0fb7ab08
Ren Jeka <renjeka@gmail.com>
2020-03-02 12:56:06 +0200

    Slick Slider in Laboratory. Working with svg roadmaps!

--- OTHERS/Test/Libraries/UI/SlickSlider/proj_1/src/main.js
+++ OTHERS/Test/Libraries/UI/SlickSlider/proj_1/src/main.js
@@ -2,8 +2,21 @@ var $ = require("jquery");
 require("slick-carousel");
 
 $(document).ready(function () {
+
+	let $dotsContainer, $dots;
+
 	$(".slides").slick({
-		arrows: true,
+		arrows: false,
 		dots: true,
+		appendDots: $("#navDots"),
 	});
+
+
+	$dots = $(".slick-dots button");
+	$dots.eq(0).text("Font-end");
+	$dots.eq(1).text("Java");
+	$dots.eq(2).text("Dot net");
+
+
+	
 });

8fac7f384b0c863295fa004a38070b303a386841
renjeka <renjeka@gmail.com>
2023-02-23 17:34:34 +0200

    modified Java  hello-lambda-core

---
+++ Java_projects/hello-lambda-core/src/main/java/website/yevhenii/MyBot.java
@@ -0,0 +1,22 @@
+package website.yevhenii;
+
+import org.telegram.telegrambots.bots.TelegramWebhookBot;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class MyBot extends TelegramWebhookBot {
+    @Override
+    public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
+        return null;
+    }
+
+    @Override
+    public String getBotPath() {
+        return null;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return null;
+    }
+}
--- Java_projects/hello-lambda-core/src/main/java/website/yevhenii/SimpleHandler.java
+++ Java_projects/hello-lambda-core/src/main/java/website/yevhenii/SimpleHandler.java
@@ -3,17 +3,36 @@ package website.yevhenii;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.apache.http.client.methods.HttpPost;
+import org.telegram.telegrambots.meta.api.objects.Update;
+//import org.apache.http.message.BasicHttpRequest;
+//import org.telegram.telegrambots.meta.api.objects.Update;
 
-public class SimpleHandler implements RequestHandler<Object, String> {
+//import java.io.InputStream;
 
+public class SimpleHandler implements RequestHandler<Update, String> {
+
+//    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     @Override
-    public String handleRequest(Object input, Context context) {
+    public String handleRequest(Update input, Context context)
+    {
+//        Update update;
         LambdaLogger logger = context.getLogger();
-        logger.log("----------------- LOG START ---------------");
-        logger.log("Function '" + context.getFunctionName() + "' called.\n");
-        logger.log(input.toString());
-        logger.log("----------------- LOG END -----------------");
-        return input.toString();
+        try {
+//            update = MAPPER.readValue(input, Update.class);
+            logger.log(input.getClass().toString());
+            logger.log("---------- START -----------");
+//            logger.log(update.toString());
+
+            logger.log(input.toString());
+            logger.log("---------- END -----------");
+        } catch (Exception e) {
+            System.err.println("Failed to parse update: " + e);
+            throw new RuntimeException("Failed to parse update!", e);
+        }
+
+        return "OK!";
     }
 }

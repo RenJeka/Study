2618782c0b8bb0584123c9175791852f9c98d9ca
renjeka <renjeka@gmail.com>
2023-02-24 18:04:57 +0200

    Java AWS Lambda telegram-bot  — get correct data (interface) from Lambda(APIGatewayV2HTTPEvent)

--- Java_projects/hello-lambda-core/src/main/java/website/yevhenii/SimpleHandler.java
+++ Java_projects/hello-lambda-core/src/main/java/website/yevhenii/SimpleHandler.java
@@ -3,30 +3,29 @@ package website.yevhenii;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import org.apache.http.client.methods.HttpPost;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.telegram.telegrambots.meta.api.objects.Update;
-//import org.apache.http.message.BasicHttpRequest;
-//import org.telegram.telegrambots.meta.api.objects.Update;
 
-//import java.io.InputStream;
+public class SimpleHandler implements RequestHandler<APIGatewayV2HTTPEvent, String> {
 
-public class SimpleHandler implements RequestHandler<Update, String> {
-
-//    private static final ObjectMapper MAPPER = new ObjectMapper();
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
-    public String handleRequest(Update input, Context context)
+    public String handleRequest(APIGatewayV2HTTPEvent inputMessage, Context context)
     {
 //        Update update;
         LambdaLogger logger = context.getLogger();
         try {
-//            update = MAPPER.readValue(input, Update.class);
-            logger.log(input.getClass().toString());
             logger.log("---------- START -----------");
-//            logger.log(update.toString());
 
-            logger.log(input.toString());
+            Update updateMessage = objectMapper.readValue(inputMessage.getBody(), Update.class);
+            logger.log("---------- getUpdateId -----------");
+            logger.log(updateMessage.getUpdateId().toString());
+            logger.log("---------- USER -----------");
+            logger.log(updateMessage.getMessage().getFrom().getFirstName() +  " " + updateMessage.getMessage().getFrom().getLastName());
+            logger.log("---------- MESSAGE -----------");
+            logger.log(updateMessage.getMessage().getText());
             logger.log("---------- END -----------");
         } catch (Exception e) {
             System.err.println("Failed to parse update: " + e);

a5beb89545d21a969455da9a513248caa6f36d5b
RenJeka <renjeka@gmail.com>
2023-02-25 15:19:26 +0200

    Java -> YevheniiJavaBot. First Successfull ьmessage send to client

--- Java_projects/hello-lambda-core/src/main/java/website/yevhenii/MyBot.java
+++ Java_projects/hello-lambda-core/src/main/java/website/yevhenii/MyBot.java
@@ -2,21 +2,56 @@ package website.yevhenii;
 
 import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+// BotName: YevheniiJavaBot
+// Bot 5960805812:AAGnFLn1XAgQLowDAGWUoVZDGrJw8SpVvrg
 
 public class MyBot extends TelegramWebhookBot {
+
     @Override
     public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
-        return null;
+        Long chatId = getChatId(update);
+
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        String userName = update.getMessage().getFrom().getFirstName() + " " + update.getMessage().getFrom().getLastName();
+        message.setText("Hello, " + userName + " !");
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
+        return message;
     }
 
     @Override
     public String getBotPath() {
-        return null;
+        return "https://b43ql3aerbzexql27d3koe3fuu0kzctp.lambda-url.us-east-1.on.aws/";
     }
 
     @Override
     public String getBotUsername() {
+        return "YevheniiJavaBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "5960805812:AAGnFLn1XAgQLowDAGWUoVZDGrJw8SpVvrg";
+    }
+
+    public Long getChatId(Update update) {
+        if (update.hasMessage()) {
+            return update.getMessage().getFrom().getId();
+        }
+
+        if (update.hasCallbackQuery()) {
+            return update.getCallbackQuery().getFrom().getId();
+        }
+
         return null;
     }
+
 }
--- Java_projects/hello-lambda-core/src/main/java/website/yevhenii/SimpleHandler.java
+++ Java_projects/hello-lambda-core/src/main/java/website/yevhenii/SimpleHandler.java
@@ -5,33 +5,42 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-public class SimpleHandler implements RequestHandler<APIGatewayV2HTTPEvent, String> {
+public class SimpleHandler implements RequestHandler<APIGatewayV2HTTPEvent, BotApiMethod<?>> {
 
     ObjectMapper objectMapper = new ObjectMapper();
+    MyBot myBot = new MyBot();
+
 
     @Override
-    public String handleRequest(APIGatewayV2HTTPEvent inputMessage, Context context)
+    public BotApiMethod<?> handleRequest(APIGatewayV2HTTPEvent inputMessage, Context context)
     {
 //        Update update;
         LambdaLogger logger = context.getLogger();
         try {
+            TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+            Update update = objectMapper.readValue(inputMessage.getBody(), Update.class);
+            BotApiMethod<?> response = myBot.onWebhookUpdateReceived(update);
             logger.log("---------- START -----------");
 
-            Update updateMessage = objectMapper.readValue(inputMessage.getBody(), Update.class);
             logger.log("---------- getUpdateId -----------");
-            logger.log(updateMessage.getUpdateId().toString());
+            logger.log(update.getUpdateId().toString());
             logger.log("---------- USER -----------");
-            logger.log(updateMessage.getMessage().getFrom().getFirstName() +  " " + updateMessage.getMessage().getFrom().getLastName());
+            logger.log(update.getMessage().getFrom().getFirstName() +  " " + update.getMessage().getFrom().getLastName());
             logger.log("---------- MESSAGE -----------");
-            logger.log(updateMessage.getMessage().getText());
+            logger.log(update.getMessage().getText());
+
+            logger.log("---------- BotApiMethod RESPONSE -----------");
+            logger.log(response.toString());
             logger.log("---------- END -----------");
+            return response;
         } catch (Exception e) {
             System.err.println("Failed to parse update: " + e);
             throw new RuntimeException("Failed to parse update!", e);
         }
-
-        return "OK!";
     }
 }

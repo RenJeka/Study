c119173ffd5d22b7246ec1e4b3fc0cc4b8f6c645
renjeka <renjeka@gmail.com>
2023-03-28 15:21:02 +0300

    NodeJS HW 5-3 do all requirenments

--- NodeJS/itvdn/5/HW/3/index.js
+++ NodeJS/itvdn/5/HW/3/index.js
@@ -11,37 +11,57 @@ const adminCredentials = {
     password: '123456'
 };
 
+const userCredentials = [];
+
 const folderPaths = {
+    pages: '/pages',
     image: '/images',
     styles: '/styles',
     js: '/js',
 };
 
 const pagesPaths = {
-    index: '/pages/index.html',
-    notFound: '/pages/404.html'
+    index: path.join(__dirname, '/pages/index.html'),
+    notFound: path.join(__dirname, '/pages/404.html'),
+    adminPage: path.join(__dirname, '/cabinetPages/adminPage.html'),
+    userPage: path.join(__dirname, '/cabinetPages/userPage.html')
 };
 
 router.route(['/', '/home', '/index.html'])
     .get((req, res) => {
-        homePageHandler(req, res)
+        res.sendFile(pagesPaths.index);
     })
 
 router.route(/\/\w*/)
     .get((req, res) => {
-        notFoundPageHandler(req, res)
+        res.sendFile(pagesPaths.notFound);
     });
 
-router.route('/data')
+router.route('/login')
     .post((req, res) => {
-        res.append('Content-Type', 'text/plain');
+        res.append('Content-Type', 'text/html');
         if (checkAdminAuthorization(req.body)) {
-            res.status(200).send('Authorized successfully!')
+            res.status(200).sendFile(pagesPaths.adminPage);
+        } else if (checkUserAuthorization(req.body)) {
+            res.status(200).sendFile(pagesPaths.userPage);
         } else {
             res.status(401).send('Bad credentials!')
         }
     });
 
+router.route('/registration')
+    .post((req, res) => {
+        res.append('Content-Type', 'text/plain');
+        if (req.body && validateCredentials(req.body)) {
+            addUserCredentials(req.body);
+            res.status(200).send('Registered!');
+        } else {
+            res.status(400).send('Unregistered!');
+        }
+
+    });
+
+app.use('/', express.static(path.join(__dirname, folderPaths.pages)));
 app.use('/', express.static(path.join(__dirname, folderPaths.image)));
 app.use('/', express.static(path.join(__dirname, folderPaths.styles)));
 app.use('/', express.static(path.join(__dirname, folderPaths.js)));
@@ -52,15 +72,23 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
 
-function homePageHandler(req, res) {
-    res.sendFile(path.join(__dirname, pagesPaths.index));
-}
-
-function notFoundPageHandler(req, res) {
-    res.append('Content-Type', 'text/html');
-    res.sendFile(path.join(__dirname, pagesPaths.notFound));
-}
-
 function checkAdminAuthorization(authData) {
     return (authData.login === adminCredentials.login) && (authData.pass === adminCredentials.password);
+}
+
+function checkUserAuthorization(authData) {
+    const searchedUser = userCredentials.find(userCredential => {
+        return userCredential.login === authData.login;
+    });
+    return searchedUser && searchedUser.pass === authData.pass;
+}
+
+function validateCredentials(body) {
+    return !!body;
+}
+
+function addUserCredentials(body) {
+    console.log('userCred: ', body);
+    
+    userCredentials.push(body);
 }

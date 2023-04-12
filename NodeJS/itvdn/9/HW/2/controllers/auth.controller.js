7b23c5772030ee5d1bacff9ab7e53db578b113e5
renjeka <renjeka@gmail.com>
2023-04-12 12:18:18 +0300

    NodeJS lesson-9 HW-2 (copied from lesson-8 HW-2)

---
+++ NodeJS/itvdn/9/HW/2/controllers/auth.controller.js
@@ -0,0 +1,82 @@
+
+const colors = require('colors');
+
+const userAuthData = [
+    {login: 'user-1', pass: '1111'},
+    {login: 'user-2', pass: '2222'},
+    {login: 'user-3', pass: '3333'}
+
+];
+
+function checkSession(req, res, next) {
+
+    if (req.originalUrl === '/login' && req.method === 'POST') {
+        next();
+    } else if (!req.session.userName) {
+        showAuthPage(req, res);
+    } else if (isUserPresent(req.session.userName)){
+        next();
+    } else {
+        res.status(401).end(`Can't find such user '${req.session.userName}'.`);
+    }
+}
+
+function showAuthPage(req, res, next) {
+    res.render('auth');
+}
+
+function authorizeUser(req, res, next) {
+
+    if (handleSession(req)) {
+        res.status(200).end('logged');
+    } else {
+        res.status(401).end(`Can\'t find user \'${req.body.login}\' with such password. Please try other!`);
+    }
+}
+
+function logout(req, res) {
+    delete req.session.userName;
+    res.render('auth');
+
+}
+
+function handleSession(req) {
+    const userData = getUserData(req.body)
+    if (userData) {
+        req.session.userName = userData.login;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function getUserData(authData) {
+
+    const userData = findCurrentUserByAuth(authData)
+
+    if (userData) {
+        return userData;
+    } else {
+        return false;
+    }
+}
+
+function isUserPresent(userLogin) {
+    return !!userAuthData.find((userData) => {
+        return userData.login === userLogin
+    });
+}
+
+function findCurrentUserByAuth(currentUserData) {
+    return userAuthData.find((userData) => {
+        return userData.login === currentUserData.login && userData.pass === currentUserData.pass
+    });
+}
+
+module.exports = {
+    checkSession,
+    showAuthPage,
+    authorizeUser,
+    logout
+};
